41065dd053b71c6fc20397f62aa204eef41b43b4
jason brown <j.grant.brown@gmail.com>
2017-01-25 00:36:18 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,31 @@
+=begin
 def alphabetize(arr)
   # code here
+    alphabet= "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+    ascii="@=\\"
+=begin
+  custom_sort= lambda (a,b) do       #can use -> instead of lambda
+    a.split(' ').each_with_index do |char,i|
+      return alphabet.index(a[i])<=> alphabet.index
+      (b[i]) if a[i] !=b[i]
+    end
+    return alphabet.index(' ') <=> alphabet.index(b[-1])
+  end
+  arr.sort(&custom_sort)
+
+  arr.sort_by{|a| a.split("").map{|letter| alphabet.index(letter)}}
+end
+=end
+
+
+#phrase="mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton", "ĝusti", "ĝusti vin","ĝuspa"
+
+
+def alphabetize(phrases)
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+
+      phrases.sort_by {|phrase|
+        phrase.chars.map { |c| alphabet.index(c)
+                         }
+                      }
 end
